af8ac40a55b3fc69d49626191af5b97722edd23e
lizzybeliy0 <lizibeliy.0@mail.ru>
2026-03-06 01:13:04 +0300

    Part1 done

---
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -0,0 +1,56 @@
+package ru.iu3.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import ru.iu3.fclient.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+        LogUsingJNI("All libraries loaded successfully");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int res = initRng();
+        byte[] key = randomBytes(16);
+        byte[] data = {22, 1, 39, 53, 77};
+        LogUsingJNI("Data: " + Arrays.toString(data));
+        LogUsingJNI("Generated key: " + Arrays.toString(key));
+
+        byte[] encrypted = encrypt(key, data);
+        LogUsingJNI("Data encrypted with 3DES: " + Arrays.toString(encrypted));
+        byte[] decrypted = decrypt(key, encrypted);
+        LogUsingJNI("Data decrypted: " + Arrays.toString(decrypted));
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public static native void LogUsingJNI(String str);
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+}
